3b5e75330d96161657cac6e19cb0419f38576f52
Alexei Penkin <alexei_penkin@mail.ru>
2023-06-14 10:02:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var swiper1 = new Swiper(".swiper1", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor: true,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination1",
+        clickable: true,
+    }
+});
+
+var swiper2 = new Swiper(".swiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor: true,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination2",
+        clickable: true,
+    }
+});
